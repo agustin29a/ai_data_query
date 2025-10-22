ee2af996a190b2246db8db21ab304593d779d116
agustin29a <agustin124@gmail.com>
2025-10-22 12:38:03 -0300

    se sube video presentacion

--- chat_sessions_nest/dist/main.js
+++ chat_sessions_nest/dist/main.js
@@ -3,10 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
+const bodyParser = require("body-parser");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
     app.enableCors();
+    app.use(bodyParser.json({ limit: '10mb' }));
+    app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
     await app.listen(4000);
     console.log('Application is running on: http://localhost:4000');
 }
--- chat_sessions_nest/src/main.ts
+++ chat_sessions_nest/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
+  // Aumentar el límite de tamaño del body
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
   
   await app.listen(4000);
   console.log('Application is running on: http://localhost:4000');
